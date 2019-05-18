770f0580d472b4463420167df948fe4d97e15478
MishkaGolota <golotamm@gmail.com>
2019-05-18 13:03:25 +0300

    Updates

---
+++ precache-manifest.039035f01a5941e2f46b4b28db0af95c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ca6d903d3bfbcf0a69dfc3ee17c537",
+    "url": "/todo-react-redux/index.html"
+  },
+  {
+    "revision": "f10dadb532a9c5524477",
+    "url": "/todo-react-redux/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/todo-react-redux/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f10dadb532a9c5524477",
+    "url": "/todo-react-redux/static/js/main.a484aea6.chunk.js"
+  },
+  {
+    "revision": "c2979c65fca7cd9446fe",
+    "url": "/todo-react-redux/static/js/runtime~main.9c2e1894.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react-redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
